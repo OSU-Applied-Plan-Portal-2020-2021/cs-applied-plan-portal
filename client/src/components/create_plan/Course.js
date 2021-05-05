c3d493b0c1c517ebf6b131315cf69955a8416670
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-05-05 15:10:58 -0700

    fixed various width for add buttons

--- client/src/components/create_plan/Course.js
+++ client/src/components/create_plan/Course.js
@@ -1,27 +1,26 @@
 /** @jsx jsx */
-import PropTypes from "prop-types"
-import { css, jsx } from "@emotion/core"
-import { Mobile, Desktop } from "../../utils/responsiveUI"
-import { SCREENWIDTH } from "../../utils/constants"
-import ErrorMessage from "../general/ErrorMessage"
-import React, { useState } from "react"
-import { useDispatch, useSelector } from 'react-redux'
-import { addCourseToPlan } from '../../redux/actions'
-import { getCoursesFromPlan } from '../../redux/selectors'
-import { useEffect } from 'react'
-import { checkCourseIsAddedAsync } from '../../utils/plan'
-import { FaCheck, FaCheckCircle, FaPlus } from 'react-icons/fa'
-import { AiOutlineCheck } from 'react-icons/ai'
-import { GiCheckMark } from 'react-icons/gi'
-
+import PropTypes from "prop-types";
+import { css, jsx } from "@emotion/core";
+import { Mobile, Desktop } from "../../utils/responsiveUI";
+import { SCREENWIDTH } from "../../utils/constants";
+import ErrorMessage from "../general/ErrorMessage";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addCourseToPlan } from "../../redux/actions";
+import { getCoursesFromPlan } from "../../redux/selectors";
+import { useEffect } from "react";
+import { checkCourseIsAddedAsync } from "../../utils/plan";
+import { FaCheck, FaCheckCircle, FaPlus } from "react-icons/fa";
+import { AiOutlineCheck } from "react-icons/ai";
+import { GiCheckMark } from "react-icons/gi";
 
 // a single course description
 function Course(props) {
-	const dispatch = useDispatch()
-	const planCourses = useSelector(getCoursesFromPlan)
+  const dispatch = useDispatch();
+  const planCourses = useSelector(getCoursesFromPlan);
 
-	const width = SCREENWIDTH.MOBILE.MAX
-	const style = css`
+  const width = SCREENWIDTH.MOBILE.MAX;
+  const style = css`
     & {
       margin-bottom: 1rem;
       background: #f4f2f1;
@@ -31,32 +30,6 @@ function Course(props) {
       @media (max-width: ${width}px) {
         margin-right: 0;
       }
-			
-    }
-
-    .add-button {
-      display: inline-block;
-      margin-left: auto;
-      padding: 1rem 1rem;
-      background: var(--color-green-500);
-      color: var(--color-green-50);
-      border-radius: 0.5rem;
-      border: none;
-			width: 96px!important;
-    }
-
-		.btn-added {
-			opacity: 60%;
-		}
-
-    details summary {
-      cursor: pointer;
-      height: unset;
-      width: 100%;
-      display: inline-flex;
-      flex-direction: row;
-      align-items: center;
-      user-select: none;
     }
 
     summary + p {
@@ -84,28 +57,50 @@ function Course(props) {
       padding: 0 5px 5px 0;
     }
 
+    details summary {
+      cursor: pointer;
+      height: unset;
+      width: 100%;
+      display: inline-flex;
+      align-items: center;
+      user-select: none;
+      justify-content: space-between;
+    }
+
     .add-button {
-      margin-right: 0;
-      margin-left: auto;
-      white-space: nowrap;
-			height: 40px;
+      /* display: inline-block; */
+      /* margin-left: auto; */
+      padding: 1rem 1rem;
+      background: var(--color-green-500);
+      color: var(--color-green-50);
+      border-radius: 0.5rem;
+      border: none;
+      width: 96px !important;
+      margin-left: 1rem;
+    }
+
+    .added-checkmark {
+      font-size: 1.7rem;
+    }
+
+    .added-button {
+      background-color: var(--color-green-300);
     }
 
     .disabled {
       background: var(--color-lightgray-700);
       color: var(--color-gray-50);
-			/* pointer-events: none; */
+      /* pointer-events: none; */
       cursor: default;
-    }
-
-    .course-title {
-      font-weight: 600;
     }
 
     .course-title {
       display: inline-block;
       vertical-align: bottom;
+      font-weight: 600;
+      flex-grow: 1;
       text-transform: uppercase;
+      width: 245px;
     }
 
     .course-code {
@@ -122,103 +117,105 @@ function Course(props) {
       margin-top: 10px;
       margin-bottom: 10px;
     }
+  `;
+  let notEligible = false;
+  if (props.restriction > 0) notEligible = true;
+  const [added, setAdded] = useState(false);
+  useEffect(() => {
+    async function checkAdded() {
+      const result = await checkCourseIsAddedAsync(props.courseId, planCourses);
+      setAdded(result);
+    }
+    checkAdded();
+  });
 
-		.added-checkmark {
-			font-size: 1.5rem;
-		}
-  `
-	let notEligible = false
-	if (props.restriction > 0) notEligible = true
-	const [added, setAdded] = useState(false)
-	useEffect(() => {
-		async function checkAdded() {
-			const result = await checkCourseIsAddedAsync(props.courseId, planCourses)
-			setAdded(result)
-		}
-		checkAdded()
-	})
+  // add the course to the plan
+  function handleAddCourse() {
+    const courseObject = {
+      courseId: props.courseId,
+      courseCode: props.courseCode,
+      courseName: props.courseName,
+      credits: props.credits,
+      description: props.description,
+      prerequisites: props.prerequisites,
+      restriction: props.restriction,
+    };
+    props.onAddCourse(courseObject);
+    setAdded(true);
+    dispatch(addCourseToPlan(courseObject));
+  }
 
-	// add the course to the plan
-	function handleAddCourse() {
-		const courseObject = {
-			courseId: props.courseId,
-			courseCode: props.courseCode,
-			courseName: props.courseName,
-			credits: props.credits,
-			description: props.description,
-			prerequisites: props.prerequisites,
-			restriction: props.restriction,
-		}
-		props.onAddCourse(courseObject)
-		setAdded(true)
-		dispatch(addCourseToPlan(courseObject))
-	}
-
-	return (
-		<div className="course" css={style}>
-			<details>
-				<summary>
-					<div className="course-title">
-						{props.courseName}
-						<div className="course-code">
-							<small>{props.courseCode}</small>
-						</div>
-					</div>
-					{notEligible
-						? <button className="add-button disabled" onClick={handleAddCourse}>
-							Not Eligible
-					</button>
-						: <button
-
-							className={`add-button ${notEligible ? "disabled" : ""}`}
-							onClick={handleAddCourse}
-						>
-							<Desktop>
-								{added ? <><span className='added-checkmark'><FaCheck /></span></> : 'Add to plan'}
-
-							</Desktop>
-							<Mobile>
-								<i class="fas fa-plus"></i>
-							</Mobile>
-						</button>
-					}
-
-
-				</summary>
-				<p>
-					{props.credits} credit hour{props.credits !== 1 && "s"}
-					{props.prerequisites === "" && ", no prerequisites"}
-				</p>
-				{props.description !== "" && (
-					<div>
-						<hr />
-						{/* <h4>Description</h4> */}
-						<p>{props.description}</p>
-					</div>
-				)}
-				{props.prerequisites !== "" && (
-					<div>
-						<hr />
-						{/* <h4>Restrictions</h4> */}
-						<p>{props.prerequisites}</p>
-					</div>
-				)}
-			</details>
-		</div>
-	)
+  return (
+    <div className="course" css={style}>
+      <details>
+        <summary>
+          <div className="course-title">
+            {props.courseName}
+            <div className="course-code">
+              <small>{props.courseCode}</small>
+            </div>
+          </div>
+          {notEligible ? (
+            <button className="add-button disabled" onClick={handleAddCourse}>
+              Not Eligible
+            </button>
+          ) : (
+            <button
+              className={`add-button ${notEligible && "disabled"} ${
+                added && "added-button"
+              }`}
+              onClick={handleAddCourse}
+            >
+              <Desktop>
+                {added ? (
+                  <>
+                    <span className="added-checkmark">
+                      <FaCheck />
+                    </span>
+                  </>
+                ) : (
+                  "Add to plan"
+                )}
+              </Desktop>
+              <Mobile>
+                <i class="fas fa-plus"></i>
+              </Mobile>
+            </button>
+          )}
+        </summary>
+        <p>
+          {props.credits} credit hour{props.credits !== 1 && "s"}
+          {props.prerequisites === "" && ", no prerequisites"}
+        </p>
+        {props.description !== "" && (
+          <div>
+            <hr />
+            {/* <h4>Description</h4> */}
+            <p>{props.description}</p>
+          </div>
+        )}
+        {props.prerequisites !== "" && (
+          <div>
+            <hr />
+            {/* <h4>Restrictions</h4> */}
+            <p>{props.prerequisites}</p>
+          </div>
+        )}
+      </details>
+    </div>
+  );
 }
 
-
-export default Course
+export default Course;
 
 Course.propTypes = {
-	courseId: PropTypes.number,
-	courseCode: PropTypes.string,
-	courseName: PropTypes.string,
-	credits: PropTypes.string,
-	description: PropTypes.string,
-	prerequisites: PropTypes.string,
-	restriction: PropTypes.number,
-	onAddCourse: PropTypes.func,
-	warning: PropTypes.string,
-}
+  courseId: PropTypes.number,
+  courseCode: PropTypes.string,
+  courseName: PropTypes.string,
+  credits: PropTypes.string,
+  description: PropTypes.string,
+  prerequisites: PropTypes.string,
+  restriction: PropTypes.number,
+  onAddCourse: PropTypes.func,
+  warning: PropTypes.string,
+};
